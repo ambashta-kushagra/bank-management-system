23a3a421c488d2677c66ce9087e91bfa16845891
Kushagra-Ambashta <ambashtakushagra@gmail.com>
2024-12-21 14:00:29 +0530

    Making Deposit Class and Started linking process to SQL

--- bank/management/system/Deposit.java
+++ bank/management/system/Deposit.java
@@ -2,19 +2,59 @@ package bank.management.system;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Deposit extends JFrame {
+public class Deposit extends JFrame implements ActionListener {
 
     String pin;
+
+    TextField textField;
+
+    JButton deposit,back;
 
     Deposit(String pin){
         this.pin = pin;
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/atm2.png"));
-        Image i2 = i1.getImage().getScaledInstance(1280,430,Image.SCALE_DEFAULT);
+        Image i2 = i1.getImage().getScaledInstance(1280,640,Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel l3 = new JLabel(i3);
-        l3.setBounds(0,0,1280,580);
+        l3.setBounds(0,0,1280,640);
         add(l3);
+
+        JLabel label1 = new JLabel("ENTER AMOUNT YOU WANT TO DEPOSIT");
+        label1.setForeground(Color.WHITE);
+        label1.setFont(new Font("System", Font.BOLD, 16));
+        label1.setBounds(360,130,400,35);
+        l3.add(label1);
+
+        textField = new TextField();
+        textField.setBackground(new Color(65,125,128));
+        textField.setForeground(Color.WHITE);
+        textField.setBounds(360,170,320,25);
+        textField.setFont(new Font("Raleway",Font.BOLD,22));
+        l3.add(textField);
+
+        deposit = new JButton("DEPOSIT");
+        deposit.setBounds(580,280,120,30);
+        deposit.setBackground(new Color(65,125,128));
+        deposit.setForeground(Color.WHITE);
+        //deposit.addActionListener(this);
+        l3.add(deposit);
+
+        back = new JButton("BACK");
+        back.setBounds(580,315,120,30);
+        back.setBackground(new Color(65,125,128));
+        back.setForeground(Color.WHITE);
+        //deposit.addActionListener(this);
+        l3.add(back);
+
+
+
+
+
+
+
 
         setLayout(null);
         setSize(1280,880);
@@ -23,6 +63,12 @@ public class Deposit extends JFrame {
 
 
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String amount = textField.getText();
+    }
+
     public static void main(String[] args) {
         new Deposit(" ");
     }
